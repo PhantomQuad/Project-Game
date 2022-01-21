a66f5a6f07874a1735ab02e69ae01e4ca6a63b97
Nathan <PhantonQuad@users.noreply.github.com>
2022-01-21 10:43:54 +0000

    code tidy

--- index.js
+++ index.js
@@ -109,6 +109,10 @@ class Character {
         }
         this._description = value;
     }
+
+    set pronoun(value) {
+        this._pronoun = value;
+      }
     
     set conversation(value) {
         if (value.length < 4) {
@@ -123,11 +127,11 @@ class Character {
       }
     
     describe(){
-        return "You have met " + this._name + ", " + this._pronoun + " is a " + this._description;
+        return "You have met " + this._name + ", " + this._pronoun + " is " + this._description;
     }
 
     converse() {
-        return this._name + " says " + "'" + this._conversation + "'";
+        return this._pronoun + " says " + "'" + this._conversation + "'";
     }
 
     items() {
@@ -202,7 +206,7 @@ class Item {
           console.log("Name of power is too short.");
           return;
         }
-        this._name = value;
+        this._power = value;
       }
     
     describe(){
@@ -228,19 +232,19 @@ ExitDeadwind.description = "the sun is just coming up over the horizon. You Well
 // create items
 const ShadowBlade = new Item("Shadow Blade");
 ShadowBlade.description = "a magical blade filled with magical powers.";
-ShadowBlade._power = "shadow";
+ShadowBlade.power = "shadow";
 
 const FrostStaff = new Item("Frost Staff");
 FrostStaff.description = "an ancient staff, it looks cold to touch.";
-FrostStaff._power = "frost";
+FrostStaff.power = "frost";
 
 const HolyStaff = new Item("Holy Staff");
 HolyStaff.description = "a magnificent and godly giant staff.";
-HolyStaff._power = "holy";
+HolyStaff.power = "holy";
 
 const DungeonKey = new Item("Dungeon Key");
 DungeonKey.description = "a key to allow you to exit the dungeon.";
-DungeonKey._power = "key";
+DungeonKey.power = "key";
 
 // link rooms
 Deadwind.linkRoom("north", Karazhan);
@@ -259,15 +263,15 @@ const Player1 = new Character("Player1");
 
 // 1st encounter
 const Sylvanas = new Character("Sylvanas Windrunner");
-Sylvanas.description = "Undead Elf";
-Sylvanas._pronoun = "she";
+Sylvanas.description = "an Undead Elf";
+Sylvanas.pronoun = "she";
 Sylvanas.conversation = "Greeting adventurer, I have something that will help you on your travels."
 Sylvanas.item = ShadowBlade;
 
 // 2nd encounter
 const Maiden = new Enemy("Maiden of Virtue");
-Maiden.description = "Giant";
-Maiden._pronoun = "she";
+Maiden.description = "a Giant";
+Maiden.pronoun = "she";
 Maiden.conversation = "You must repent for your sins!!!";
 Maiden.weakness = "shadow";
 Maiden.item = HolyStaff;
@@ -275,15 +279,15 @@ Maiden.item = HolyStaff;
 // 3rd encounter
 const Curator = new Enemy("The Curator");
 Curator.description = "Mechanical";
-Curator._pronoun = "he";
+Curator.pronoun = "he";
 Curator.conversation = "You shouldn't have come here!!!";
 Curator.weakness = "holy";
 Curator.item = FrostStaff;
 
 // final encounter
 const Attumen = new Enemy("Attumen the Huntsman");
-Attumen.description = "Undead";
-Attumen._pronoun = "he";
+Attumen.description = "an Undead";
+Attumen.pronoun = "he";
 Attumen.conversation = "You're no match for my fiery warhorse Midnight!";
 Attumen.weakness = "frost";
 Attumen.item = DungeonKey;
@@ -327,6 +331,46 @@ function displayRoomInfo(room) {
     document.getElementById("usertext").focus();
 }
 
+function take(room) {
+    if (room.character.item && room.character.weakness === undefined){
+        Player1.item = room.character.item;
+        room.character.item = "";
+        document.getElementById("feedbackArea").innerHTML = "You take " + room.character.name + "'s " + Player1.item.name;
+        displayRoomInfo(room);
+    } else {
+        if (room.character.weakness !== undefined){
+            document.getElementById("feedbackArea").innerHTML = "HA HA HA nice try!!! " + room.character.name + " says fight me!!!";
+            displayRoomInfo(room);
+        } else {
+            document.getElementById("feedbackArea").innerHTML = "Take what from who exactly? There is no one here.";
+            displayRoomInfo(room);
+        }
+    }
+}
+
+function startFight(room){
+    if (room.character.fight(Player1.item.power)){
+
+        textContent = "<div class='col' id='col1-1'>" + "You defeated " + room.character.name + " using " + Player1.item.name + "</div></br>" +
+        "<div class='col' id='col1-2'>" + "You overlook the body and notice " + room.character.item.description + "</div></br>" +
+        "<div class='col' id='col1-3'>" + "You can only carry one item, you grab the <span id='item'>" + room.character.item.name + "</span> and drop the <span id='item'>" + Player1.item.name + "</span></div>" +
+        "<div class='col' id='col1-4'>" + "Something tells me this will come in handy..." + "</div></br>" +
+        "<div class='col' id='col1-5'>" + room.getDetails() + "</div>";
+
+        document.getElementById("textarea").innerHTML = textContent;
+        document.getElementById("buttonarea").innerHTML = '><input type="text" id="usertext" />';
+        document.getElementById("usertext").focus();
+
+        Player1.item = room.character.item;
+        room.character = "";
+        
+    } else {
+        document.getElementById("textarea").innerHTML = "<span id='died'>You Died!!! Refresh to try again.</span>";
+        document.getElementById("buttonarea").innerHTML = '><input type="text" id="usertext" />';
+        document.getElementById("usertext").focus();
+    }
+}
+
 // Start the game
 function startGame(){
     currentRoom = Deadwind;
@@ -352,42 +396,9 @@ function startGame(){
                 displayRoomInfo(currentRoom);
             }
         } else if (command === "take") {
-            if (currentRoom.character === Sylvanas){
-                Player1.item = currentRoom.character.item;
-                currentRoom.character.item = "";
-                document.getElementById("feedbackArea").innerHTML = "You take " + currentRoom.character.name + "'s " + Player1.item.name;
-                displayRoomInfo(currentRoom);
-            } else {
-                if (currentRoom.character){
-                    document.getElementById("feedbackArea").innerHTML = "HA HA HA nice try!!! Fight me!!!";
-                    displayRoomInfo(currentRoom);
-                } else {
-                    document.getElementById("feedbackArea").innerHTML = "Take what from who exactly?";
-                    displayRoomInfo(currentRoom);
-                }
-            }
-        } else if (command === "fight"){
-            if (currentRoom.character.fight(Player1.item.power)){
-
-                textContent = "<div class='col' id='col1-1'>" + "You defeated " + currentRoom.character.name + " using " + Player1.item.name + "</div></br>" +
-                "<div class='col' id='col1-2'>" + "You overlook the body and notice " + currentRoom.character.item.description + "</div></br>" +
-                "<div class='col' id='col1-3'>" + "You can only carry one item, you grab the <span id='item'>" + currentRoom.character.item.name + "</span> and drop the <span id='item'>" + Player1.item.name + "</span></div>" +
-                "<div class='col' id='col1-4'>" + "Something tells me this will come in handy..." + "</div></br>" +
-                "<div class='col' id='col1-5'>" + currentRoom.getDetails() + "</div>";
-
-                document.getElementById("textarea").innerHTML = textContent;
-                document.getElementById("buttonarea").innerHTML = '><input type="text" id="usertext" />';
-                document.getElementById("usertext").focus();
-
-                Player1.item = currentRoom.character.item;
-                currentRoom.character = "";
-                
-            } else {
-                document.getElementById("textarea").innerHTML = "<span id='died'>You Died!!! Refresh to try again.</span>";
-                document.getElementById("buttonarea").innerHTML = '><input type="text" id="usertext" />';
-                document.getElementById("usertext").focus();
-                return;
-            }
+            take(currentRoom);
+        } else if (command === "fight" && currentRoom.character.weakness !== undefined){
+            startFight(currentRoom);
         } else {
             document.getElementById("usertext").value = "";
             document.getElementById("feedbackArea").innerHTML = "that is not a valid command please try again";
